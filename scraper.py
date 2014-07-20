591fc3af22bc8da053a28aa8f2278c749dad8542
zachbb <zach.bastick@gmail.com>
2014-07-20 19:05:14 -0400

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -8,14 +8,13 @@ import urllib2
 
 
 QUERY = '#prochoice OR #prolife'
-GEOINFO = '53.26521293124656,-9.063720703125,257km'
-RESULTS_PER_PAGE = '100'
+RESULTS_PER_PAGE = '15'
 LANGUAGE = 'en'
-NUM_PAGES = 15 
+NUM_PAGES = 1 
 
 for page in range(1, NUM_PAGES+1):
-    base_url = 'http://search.twitter.com/search.json?q=%s&geocode=%s&rpp=%s&lang=%s&page=%s' \
-         % (urllib2.quote(QUERY), urllib2.quote(GEOINFO), RESULTS_PER_PAGE, LANGUAGE, page)
+    base_url = 'http://search.twitter.com/search.json?q=%s&&rpp=%s&lang=%s&page=%s' \
+         % (urllib2.quote(QUERY), RESULTS_PER_PAGE, LANGUAGE, page)
     try:
         results_json = simplejson.loads(scraperwiki.scrape(base_url))
         for result in results_json['results']:
@@ -23,8 +22,10 @@ for page in range(1, NUM_PAGES+1):
             data['id'] = result['id']
             data['text'] = result['text']
             data['from_user'] = result['from_user']
+            data['from_user_id'] = result['from_user_id']
+            data['created_at'] = result['created_at']
             print data['from_user'], data['text']
-            scraperwiki.sqlite.save(["id"], text, data) 
+            scraperwiki.sqlite.save(["id"], data) 
     except:
         print 'Oh dear, failed to scrape %s' % base_url
         
